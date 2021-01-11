7b037d864368d1215e60fe07ed9e4723aa55be8f
Adam Wieliczko <adam.wieliczko@appwise.pl>
2021-01-11 13:36:54 +0100

    Initial commit

---
+++ static/js/53.24e9d691.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-app-tabs"]=this["webpackJsonpreact-app-tabs"]||[]).push([[53],{812:function(t,n,e){"use strict";e.r(n),e.d(n,"startStatusTap",(function(){return a}));var o=e(32),a=function(){var t=window;t.addEventListener("statusTap",(function(){Object(o.h)((function(){var n=t.innerWidth,e=t.innerHeight,a=document.elementFromPoint(n/2,e/2);if(a){var c=a.closest("ion-content");c&&c.componentOnReady().then((function(){Object(o.f)((function(){return c.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=53.24e9d691.chunk.js.map
