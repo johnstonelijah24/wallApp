3f9ac435415cb5b7e467a4affb388d804346d0ff
Adam Wieliczko <adam.wieliczko@appwise.pl>
2021-01-11 13:29:55 +0100

    Initial commit

---
+++ public/precache-manifest.298f12b723866291fa26eea6b1641cff.js
@@ -0,0 +1,274 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "364086b7430ae2b9725131741eb4e122",
+    "url": "/softwarebrothersapp.b4a.app/index.html"
+  },
+  {
+    "revision": "11c5b803d0ff3b2380c0",
+    "url": "/softwarebrothersapp.b4a.app/static/css/4.21302a68.chunk.css"
+  },
+  {
+    "revision": "abf2ed8de0a4965b997a",
+    "url": "/softwarebrothersapp.b4a.app/static/css/main.109b07fd.chunk.css"
+  },
+  {
+    "revision": "d7360c57a9d6fab2c64e",
+    "url": "/softwarebrothersapp.b4a.app/static/js/0.b882a476.chunk.js"
+  },
+  {
+    "revision": "c247fd3ee4a3eadcfe63",
+    "url": "/softwarebrothersapp.b4a.app/static/js/10.2494387c.chunk.js"
+  },
+  {
+    "revision": "114d7f4f28503b648689",
+    "url": "/softwarebrothersapp.b4a.app/static/js/11.5495d1eb.chunk.js"
+  },
+  {
+    "revision": "678126dc015e4c526f6b",
+    "url": "/softwarebrothersapp.b4a.app/static/js/12.dc92fd49.chunk.js"
+  },
+  {
+    "revision": "c143393e969240530a4f",
+    "url": "/softwarebrothersapp.b4a.app/static/js/13.9f640824.chunk.js"
+  },
+  {
+    "revision": "3ceaa11b452f45746ae4",
+    "url": "/softwarebrothersapp.b4a.app/static/js/14.dfda65dd.chunk.js"
+  },
+  {
+    "revision": "d1208c253f3beb9ee0c0",
+    "url": "/softwarebrothersapp.b4a.app/static/js/15.b9a48192.chunk.js"
+  },
+  {
+    "revision": "dddd18cfe60aa2701727",
+    "url": "/softwarebrothersapp.b4a.app/static/js/16.5d8d9bf1.chunk.js"
+  },
+  {
+    "revision": "fb5624ff216b9be98c20",
+    "url": "/softwarebrothersapp.b4a.app/static/js/17.83148ec7.chunk.js"
+  },
+  {
+    "revision": "c2387d85c48f8494f187",
+    "url": "/softwarebrothersapp.b4a.app/static/js/18.0cdadd6c.chunk.js"
+  },
+  {
+    "revision": "db5cd3d5740b5a07e10c",
+    "url": "/softwarebrothersapp.b4a.app/static/js/19.455390d5.chunk.js"
+  },
+  {
+    "revision": "70891f432e78f15d47b8",
+    "url": "/softwarebrothersapp.b4a.app/static/js/20.37b33fdd.chunk.js"
+  },
+  {
+    "revision": "8b02a3ca300c5630c0b8",
+    "url": "/softwarebrothersapp.b4a.app/static/js/21.c91938c4.chunk.js"
+  },
+  {
+    "revision": "90e49aa9d530612c4234",
+    "url": "/softwarebrothersapp.b4a.app/static/js/22.5e0cd346.chunk.js"
+  },
+  {
+    "revision": "6eaf5b543518217230b6",
+    "url": "/softwarebrothersapp.b4a.app/static/js/23.389c2a1d.chunk.js"
+  },
+  {
+    "revision": "f8b99039bf3c5b75ebd1",
+    "url": "/softwarebrothersapp.b4a.app/static/js/24.45a76b90.chunk.js"
+  },
+  {
+    "revision": "cd99c48c2564cddf70b0",
+    "url": "/softwarebrothersapp.b4a.app/static/js/25.1b2704a0.chunk.js"
+  },
+  {
+    "revision": "e1f058d80e063640fcaa",
+    "url": "/softwarebrothersapp.b4a.app/static/js/26.e32dd7c9.chunk.js"
+  },
+  {
+    "revision": "ed33e570aceee7b90310",
+    "url": "/softwarebrothersapp.b4a.app/static/js/27.2b38ac59.chunk.js"
+  },
+  {
+    "revision": "62136abddbe340cac4f3",
+    "url": "/softwarebrothersapp.b4a.app/static/js/28.ecd131db.chunk.js"
+  },
+  {
+    "revision": "c1336d168be8d8aebb5a",
+    "url": "/softwarebrothersapp.b4a.app/static/js/29.f3f0867f.chunk.js"
+  },
+  {
+    "revision": "05165df2b4cc21f999a4",
+    "url": "/softwarebrothersapp.b4a.app/static/js/30.cc4bc321.chunk.js"
+  },
+  {
+    "revision": "d7ded23ebfc9189f15b3",
+    "url": "/softwarebrothersapp.b4a.app/static/js/31.1b71b945.chunk.js"
+  },
+  {
+    "revision": "b1e91c7111db602e7a38",
+    "url": "/softwarebrothersapp.b4a.app/static/js/32.a0b7181b.chunk.js"
+  },
+  {
+    "revision": "7944f1ecf576e71d025e",
+    "url": "/softwarebrothersapp.b4a.app/static/js/33.d96452d9.chunk.js"
+  },
+  {
+    "revision": "cb2d61cbb7dff56da6bf",
+    "url": "/softwarebrothersapp.b4a.app/static/js/34.03ab924e.chunk.js"
+  },
+  {
+    "revision": "889619d5e1ef0b59bbcc",
+    "url": "/softwarebrothersapp.b4a.app/static/js/35.6550983d.chunk.js"
+  },
+  {
+    "revision": "0a3e3acff289a181a156",
+    "url": "/softwarebrothersapp.b4a.app/static/js/36.0b8baa9a.chunk.js"
+  },
+  {
+    "revision": "da3dc1550575faaeb444",
+    "url": "/softwarebrothersapp.b4a.app/static/js/37.ceb6ba98.chunk.js"
+  },
+  {
+    "revision": "ee668a11d27396c97cb9",
+    "url": "/softwarebrothersapp.b4a.app/static/js/38.7d0874b2.chunk.js"
+  },
+  {
+    "revision": "c99e76f8403fe4cd8b0d",
+    "url": "/softwarebrothersapp.b4a.app/static/js/39.a6c230c2.chunk.js"
+  },
+  {
+    "revision": "11c5b803d0ff3b2380c0",
+    "url": "/softwarebrothersapp.b4a.app/static/js/4.220e9f20.chunk.js"
+  },
+  {
+    "revision": "b0e3722e2d795cf684a3f88f0764f92b",
+    "url": "/softwarebrothersapp.b4a.app/static/js/4.220e9f20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "858e50944c4377dd76f8",
+    "url": "/softwarebrothersapp.b4a.app/static/js/40.89d4d96f.chunk.js"
+  },
+  {
+    "revision": "73f7b5cfc05ff812fa3b",
+    "url": "/softwarebrothersapp.b4a.app/static/js/41.44b07bcf.chunk.js"
+  },
+  {
+    "revision": "82ba887ad5025bfceb43",
+    "url": "/softwarebrothersapp.b4a.app/static/js/42.3d9ecd3f.chunk.js"
+  },
+  {
+    "revision": "e20297116321e7add4e6",
+    "url": "/softwarebrothersapp.b4a.app/static/js/43.e3c7a143.chunk.js"
+  },
+  {
+    "revision": "7c12af40157465331a7a",
+    "url": "/softwarebrothersapp.b4a.app/static/js/44.09d01099.chunk.js"
+  },
+  {
+    "revision": "3a9902a1dbecb7a09f61",
+    "url": "/softwarebrothersapp.b4a.app/static/js/45.8c74507d.chunk.js"
+  },
+  {
+    "revision": "41b80c26d520fa133845",
+    "url": "/softwarebrothersapp.b4a.app/static/js/46.8277c162.chunk.js"
+  },
+  {
+    "revision": "04e2adb41549307826bb",
+    "url": "/softwarebrothersapp.b4a.app/static/js/47.bc0b21f2.chunk.js"
+  },
+  {
+    "revision": "ed6e61a3796374a962f1",
+    "url": "/softwarebrothersapp.b4a.app/static/js/48.54564c06.chunk.js"
+  },
+  {
+    "revision": "87cb3700f1a806709723",
+    "url": "/softwarebrothersapp.b4a.app/static/js/49.23ac272c.chunk.js"
+  },
+  {
+    "revision": "f7ea12275c3ce6d1273b",
+    "url": "/softwarebrothersapp.b4a.app/static/js/5.18fa4513.chunk.js"
+  },
+  {
+    "revision": "f60012cb805fc5108b4c",
+    "url": "/softwarebrothersapp.b4a.app/static/js/50.33f25d02.chunk.js"
+  },
+  {
+    "revision": "b2bac319f444ac63a000",
+    "url": "/softwarebrothersapp.b4a.app/static/js/51.5c72159b.chunk.js"
+  },
+  {
+    "revision": "e438891c541e01e2c534",
+    "url": "/softwarebrothersapp.b4a.app/static/js/52.11eaaeb1.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/softwarebrothersapp.b4a.app/static/js/52.11eaaeb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "defbe8f5cf1ab3ee9121",
+    "url": "/softwarebrothersapp.b4a.app/static/js/53.24e9d691.chunk.js"
+  },
+  {
+    "revision": "43ab84c91e1cc4864536",
+    "url": "/softwarebrothersapp.b4a.app/static/js/54.28a27f0c.chunk.js"
+  },
+  {
+    "revision": "007e1f74f3f924fa8d1c",
+    "url": "/softwarebrothersapp.b4a.app/static/js/55.9cf91a5c.chunk.js"
+  },
+  {
+    "revision": "42f7882ad5dee2c96b6a",
+    "url": "/softwarebrothersapp.b4a.app/static/js/56.0babd258.chunk.js"
+  },
+  {
+    "revision": "cd16d7a044b4b7b4264d",
+    "url": "/softwarebrothersapp.b4a.app/static/js/57.54bd43f2.chunk.js"
+  },
+  {
+    "revision": "cb8248d945d00cc23ca4",
+    "url": "/softwarebrothersapp.b4a.app/static/js/58.052a1a80.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/softwarebrothersapp.b4a.app/static/js/58.052a1a80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b88e4a8f509031f343f",
+    "url": "/softwarebrothersapp.b4a.app/static/js/6.63fb3bcd.chunk.js"
+  },
+  {
+    "revision": "8ee6d7ddb796f22e2f2e",
+    "url": "/softwarebrothersapp.b4a.app/static/js/7.e20e8aee.chunk.js"
+  },
+  {
+    "revision": "14cdf69f3419d28f33a2",
+    "url": "/softwarebrothersapp.b4a.app/static/js/8.bb35b84c.chunk.js"
+  },
+  {
+    "revision": "611d8e4442e244e74877",
+    "url": "/softwarebrothersapp.b4a.app/static/js/9.61f5b12c.chunk.js"
+  },
+  {
+    "revision": "abf2ed8de0a4965b997a",
+    "url": "/softwarebrothersapp.b4a.app/static/js/main.35463ff2.chunk.js"
+  },
+  {
+    "revision": "dcd51f254d0751dea6c8",
+    "url": "/softwarebrothersapp.b4a.app/static/js/polyfills-dom.1a51d417.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/softwarebrothersapp.b4a.app/static/js/polyfills-dom.1a51d417.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a346d1dc69db118d3b0e",
+    "url": "/softwarebrothersapp.b4a.app/static/js/runtime-main.62cc53ec.js"
+  },
+  {
+    "revision": "2c7c0127ed34efcde236d3cd719e4cfd",
+    "url": "/softwarebrothersapp.b4a.app/static/media/drinks.2c7c0127.jpg"
+  },
+  {
+    "revision": "a074c77726066ecd2a64786737f350a3",
+    "url": "/softwarebrothersapp.b4a.app/static/media/food.a074c777.jpg"
+  }
+]);
